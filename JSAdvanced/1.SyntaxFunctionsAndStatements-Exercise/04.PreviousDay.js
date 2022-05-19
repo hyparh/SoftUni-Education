04b520d2b9a1a32a4262802ca97ba485cf060929
hyparh <hyparh@gmail.com>
2022-05-19 23:19:53 +0300

    update

--- JSAdvanced/1.SyntaxFunctionsAndStatements-Exercise/04.PreviousDay.js
+++ JSAdvanced/1.SyntaxFunctionsAndStatements-Exercise/04.PreviousDay.js
@@ -1,6 +1,6 @@
 function previousDay(year, month, day) {
     var date = new Date(year, month - 1, day - 1).toISOString().replace(/T.*/,'').replace(/\b0/g,'').split('-').join('-');
-
+    
     console.log(date);
 }
 
