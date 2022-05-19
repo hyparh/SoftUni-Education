5d46d9fba54e693f173794fa9c9bd8e8414391bd
hyparh <hyparh@gmail.com>
2022-05-19 23:19:30 +0300

    TimeToWalk solved

--- JSAdvanced/1.SyntaxFunctionsAndStatements-Exercise/05.TimeToWalk.js
+++ JSAdvanced/1.SyntaxFunctionsAndStatements-Exercise/05.TimeToWalk.js
@@ -1,12 +1,14 @@
 function timeToWalk(steps, stepLength, speed) {
-    let metersWalked = stepLength * speed * 1000;
-    let stepsWalked = metersWalked * stepLength;
+    let distanceInMeters = steps * stepLength;
+    let metersPerSecond = speed / 3.6; // converting from km/h to m/s
+    let time = distanceInMeters / metersPerSecond;
+    let rest = Math.floor(distanceInMeters / 500);
 
-    let timeInSeconds = steps / metersPerSecond;
+    let timeMin = Math.floor(time / 60);
+    let timeSec = Math.round(time - (timeMin * 60));
+    let timeHours = Math.floor(time / 3600);
 
-    let timeInMinutes = steps - timeInSeconds;
-
-    //changes
+    console.log((timeHours < 10 ? "0" : "") + timeHours + ":" + (timeMin < 10 ? "0" : "") + (timeMin + rest) + ":" + (timeSec < 10 ? "0" : "") + timeSec);
 }
 
 timeToWalk(4000, 0.60, 5);
