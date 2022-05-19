d554afd66df7f2f9437743e94f5b87132ed6f42d
hyparh <hyparh@gmail.com>
2022-05-19 23:18:47 +0300

    ValidityChecker solved

--- JSAdvanced/1.SyntaxFunctionsAndStatements-Exercise/08.ValidityChecker.js
+++ JSAdvanced/1.SyntaxFunctionsAndStatements-Exercise/08.ValidityChecker.js
@@ -1,11 +1,26 @@
 function validityChecker(x1, y1, x2, y2) {
-    x1 = Number(x1);
-    y1 = Number(y1);
-    x2 = Number(x2);
-    y2 = Number(y2);
-
-    let formula = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-
+ 
+    let formula1 = Math.sqrt(Math.pow(0 - x1, 2) + Math.pow(0 - y1, 2));
+    let formula2 = Math.sqrt(Math.pow(x2 - 0, 2) + Math.pow(y2 - 0, 2));
+    let formula3 = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+ 
+    if (formula1 % 1 == 0) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+ 
+    if (formula2 % 1 == 0) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+ 
+    if (formula3 % 1 == 0) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
 }
 
 validityChecker(3, 0, 0, 4);
