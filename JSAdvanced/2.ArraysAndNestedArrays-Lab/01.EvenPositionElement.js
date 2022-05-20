2f1f6905cbab6b519f3c09fa202f53353a2bc392
hyparh <hyparh@gmail.com>
2022-05-20 10:18:56 +0300

    task 1 solved

--- JSAdvanced/2.ArraysAndNestedArrays-Lab/01.EvenPositionElement.js
+++ JSAdvanced/2.ArraysAndNestedArrays-Lab/01.EvenPositionElement.js
@@ -1,6 +1,14 @@
-function evenPositionElement() {
-    
+function evenPositionElement(input) {
+    let arrResult = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (i % 2 === 0) {
+            arrResult.push(input[i]);
+        }        
+    }
+
+    console.log(arrResult.join(' '));
 }
 
-function evenPositionElement();
-function evenPositionElement();
+evenPositionElement(['20', '30', '40', '50', '60']);
+evenPositionElement(['5', '10']);
