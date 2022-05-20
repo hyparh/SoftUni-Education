273bc37a0450a0a26d7e12170c4c8a62f18727cc
hyparh <hyparh@gmail.com>
2022-05-20 13:23:45 +0300

    update

--- JSAdvanced/2.ArraysAndNestedArrays-Lab/05.SmallestTwoNumbers.js
+++ JSAdvanced/2.ArraysAndNestedArrays-Lab/05.SmallestTwoNumbers.js
@@ -1,7 +1,7 @@
 function smallestTwoNumbers(arr) {
 
     let result = arr
-        .sort((a,b) => a - b)
+        .sort((a, b) => a - b)
         .slice(0, 2);
 
     console.log(result.join(' '));
