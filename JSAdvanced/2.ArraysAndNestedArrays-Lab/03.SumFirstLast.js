661287a9d5d3d594c40fc099a5329e773738c6c0
hyparh <hyparh@gmail.com>
2022-05-20 13:06:04 +0300

    task 3 solved

---
+++ JSAdvanced/2.ArraysAndNestedArrays-Lab/03.SumFirstLast.js
@@ -0,0 +1,9 @@
+function sumFirstLast(arr) {
+    let n1 = Number(arr[0]);
+    let n2 = Number(arr[arr.length - 1]);
+    
+    console.log(n1 + n2);
+}
+
+sumFirstLast(['20', '30', '40']);
+sumFirstLast(['5', '10']);
