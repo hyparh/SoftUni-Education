73b80d5957183c4bdabb8c783102a77c36c075af
hyparh <hyparh@gmail.com>
2022-05-23 21:32:00 +0300

    solved task 02 by different approach

--- JSAdvanced/2.ArraysAndNestedArrays-Exercise/02.EveryNthElementOfAnArray.js
+++ JSAdvanced/2.ArraysAndNestedArrays-Exercise/02.EveryNthElementOfAnArray.js
@@ -1,7 +1,20 @@
 function everyNthElementOfArray(arr, step) {
-    // for i+=step
+    let result = [];
 
-    return arr.filter((element, index) => index % step == 0);
+    for (let i = 0; i <= arr.length - 1; i+=step) {
+
+        if (i === 0) {
+            result.push(arr[i]);
+            continue;
+        }
+
+        result.push(arr[i]);
+    }
+
+    return result;
+
+    // short solution
+    // return arr.filter((element, index) => index % step == 0);
 }
 
 everyNthElementOfArray(['5', '20', '31', '4', '20'], 2);
