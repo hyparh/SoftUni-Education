9db933c25522c5754f79a3da722de9c5b6b5b37d
hyparh <hyparh@gmail.com>
2022-05-28 15:57:45 +0300

    test task

---
+++ JSAdvanced/Test/MetodaSom.js
@@ -0,0 +1,9 @@
+function som() {
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    let num3 = document.getElementById('num3').value;
+
+    let sum = Number(num1) + Number(num2) + Number(num3);
+
+    document.getElementById('sum').value = sum;
+}
