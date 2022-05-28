be399daa177936e3d108c520e781d54f71b71744
hyparh <hyparh@gmail.com>
2022-05-28 15:57:18 +0300

    DOMIntroduction-Lab task 4 solved

--- JSAdvanced/4.DOMIntroduction-Lab/02.CollectListItems/CollectListItems.js
+++ JSAdvanced/4.DOMIntroduction-Lab/02.CollectListItems/CollectListItems.js
@@ -1,15 +1,6 @@
 function extractText() {
-    let itemNodes = document.querySelectorAll("ul#items li");
-    let textarea = document.querySelector("#result");
+    let items = Array.from(document.querySelectorAll('li'));
+    let result = items.map(e => e.textContent).join('\n');
 
-    for (let node of itemNodes) {
-        textarea.value += node.textContent + "\n";
-    }
-
-    // variant II (does not work correctly in Judge)
-
-    // const items = Array.from(document.querySelectorAll('li'));
-    // const result = item.map(e => e.textContent).join('\n');
-
-    // document.getElementById('result').value = result;
+    document.getElementById('result').value = result;
 }
