dd70e138d7d07a685b4afeb35eee47b154958d34
hyparh <hyparh@gmail.com>
2022-05-28 16:18:36 +0300

    DOMIntroduction-Lab task 5 solved

--- JSAdvanced/4.DOMIntroduction-Lab/04.Show More/ShowMore.js
+++ JSAdvanced/4.DOMIntroduction-Lab/04.Show More/ShowMore.js
@@ -1,4 +1,4 @@
 function showText() {
-    document.getElementById('text').style.display = 'inline';
-    document.getElementById('more').style.display = 'none';
+    document.getElementById('text').style.display = 'inline'; // 2. ...and then this one appears instead
+    document.getElementById('more').style.display = 'none'; // 1. On click this one disappears...
 }
