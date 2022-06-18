a7e8668745c32be428ffab888ad572f2c11a8efd
hyparh <hyparh@gmail.com>
2022-06-18 18:04:31 +0300

    Renaming folder

---
+++ C#Algorithms/1.RecursionAndBacktracking-lab/02.RecursiveDrawing/RecursiveDrawing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RecursiveDrawing
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            DrawFigure(n);
+        }
+
+        private static void DrawFigure(int row)
+        {
+            if (row == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', row)); // pre-action
+
+            DrawFigure(row - 1);
+
+            // this code is reached when we hit the bottom by the recursion
+            // at this point on it starts going backwards drawing the #s
+            Console.WriteLine(new string('#', row)); // post-action
+        }
+    }
+}
---
+++ C#Algorithms/1.RecursionAndBacktracking-lab/05.FindAllPathsInALabyrinth/FindAllPathsInALabyrinth/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAllPathsInALabyrinth
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            var labyrinth = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var colElement = Console.ReadLine();
+
+                for (int col = 0; col < colElement.Length; col++)
+                {
+                    labyrinth[row, col] = colElement[col];
+                }
+            }
+
+            FindPaths(labyrinth, 0, 0, new List<string>(), "");
+        }
+
+        private static void FindPaths(char[,] labyrinth, int row, int col, List<string> directions, string direction)
+        {
+            // validates row and col
+            if (row < 0 || row >= labyrinth.GetLength(0) || col < 0 || col >= labyrinth.GetLength(1))
+            {
+                return;
+            }
+
+            // check for walls or already visited cells
+            if (labyrinth[row, col] == '*' || labyrinth[row, col] == 'v')
+            {
+                return;
+            }
+
+            directions.Add(direction);
+
+            // check for End
+            if (labyrinth[row, col] == 'e')
+            {
+                Console.WriteLine(string.Join("", directions));
+                directions.RemoveAt(directions.Count - 1);
+
+                return;
+            }
+
+            labyrinth[row, col] = 'v';            
+
+            FindPaths(labyrinth, row - 1, col, directions, "U"); // Up
+            FindPaths(labyrinth, row + 1, col, directions, "D"); // Down
+            FindPaths(labyrinth, row, col - 1, directions, "L"); // Left
+            FindPaths(labyrinth, row, col + 1, directions, "R"); // Right
+
+            labyrinth[row, col] = '-';
+            directions.RemoveAt(directions.Count - 1);
+        }
+    }
+}
