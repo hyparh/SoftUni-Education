f0b6c89673eb65efffc963f1916f78c699513546
hyparh <hyparh@gmail.com>
2022-06-29 20:34:35 +0300

    Tasks 2, 3, 4, 6, 7 and 8 solved

---
+++ C#Algorithms/4.SearchingSortingAndGreedyAlgorithms-lab/04.InsertionSort/04.InsertionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.BinarySearch
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            InsertionSort(numbers);
+
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+
+        private static void InsertionSort(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                var j = i;
+
+                while (j > 0 && numbers[j - 1] > numbers[j])
+                {
+                    Swap(numbers, j, j - 1);
+                    j--;
+                }
+            }
+        }
+
+        private static void Swap(int[] numbers, int first, int second)
+        {
+            var temp = numbers[first];
+            numbers[first] = numbers[second];
+            numbers[second] = temp;
+        }
+    }
+}
