9581b74f442cc332e76cc0a9872c58b39ed79cd3
hyparh <hyparh@gmail.com>
2022-07-09 15:15:13 +0300

    All tasks solved

---
+++ C#Algorithms/8.IntroductionToDynamicProgramming-exercise/01.BinomialCoefficients/01.BinomialCoefficients/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.BinomialCoefficients
+{
+    internal class Program
+    {
+        // this one we use for the memorization
+        private static Dictionary<string, long> cache;
+
+        static void Main()
+        {
+            var row = int.Parse(Console.ReadLine());
+            var col = int.Parse(Console.ReadLine());
+
+            cache = new Dictionary<string, long>();
+
+            Console.WriteLine(GetBinom(row, col));
+        }
+
+        private static long GetBinom(int row, int col)
+        {
+            if (col == 0 || col == row)
+            {
+                return 1;
+            }
+
+            var key = $"{row}-{col}";
+
+            if (cache.ContainsKey(key))
+            {
+                return cache[key];
+            }
+
+            var result = GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+            cache[key] = result;
+
+            return result;
+        }
+    }
+}
---
+++ C#Algorithms/8.IntroductionToDynamicProgramming-exercise/04.SumWithLimitedAmountOfCoins/04.SumWithLimitedAmountOfCoins/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SumWithLimitedAmountOfCoins
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountSum(numbers, target));
+        }
+
+        private static int CountSum(int[] numbers, int target)
+        {
+            var count = 0;
+            var sums = new HashSet<int> { 0 };
+
+            foreach (var number in numbers)
+            {
+                var newSums = new HashSet<int>();
+
+                foreach (var sum in sums)
+                {
+                    var newSum = sum + number;
+
+                    if (newSum == target)
+                    {
+                        count++;
+                    }
+
+                    newSums.Add(newSum);
+                }
+
+                sums.UnionWith(newSums);
+            }
+
+            return count;
+        }
+    }
+}
