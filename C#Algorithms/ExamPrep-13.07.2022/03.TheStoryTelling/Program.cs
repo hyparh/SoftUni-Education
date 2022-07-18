9d96c1a4779f33cbcd04e0ada1e0c8cee570f57c
hyparh <hyparh@gmail.com>
2022-07-18 18:22:00 +0300

    Exam prep

---
+++ C#Algorithms/ExamPrep-13.07.2022/03.TheStoryTelling/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.TheStoryTelling
+{
+    public class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static HashSet<string> passedNodes;
+
+        static void Main()
+        {
+            ReadInputAndFillTheGraph();
+
+            passedNodes = new HashSet<string>();
+
+            foreach (var parentNode in graph.Keys)
+            {
+                DFS(parentNode);
+            }
+
+            Console.WriteLine(String.Join(" ", passedNodes.Reverse()));
+        }
+
+        private static void ReadInputAndFillTheGraph()
+        {
+            graph = new Dictionary<string, List<string>>();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArgs = command.Split("->", StringSplitOptions.RemoveEmptyEntries);
+
+                string preStory = cmdArgs[0].Trim();
+
+                // If the node is first time occured
+                if (!graph.ContainsKey(preStory))
+                {
+                    graph[preStory] = new List<string>();
+                }
+
+                // No post stories
+                if (cmdArgs.Length < 2)
+                {
+                    continue;
+                }
+
+                string[] postStories = cmdArgs[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                graph[preStory].AddRange(postStories);
+            }
+        }
+
+        private static void DFS(string parentNode)
+        {
+            if (passedNodes.Contains(parentNode))
+            {
+                return;
+            }
+
+            foreach (var childNode in graph[parentNode])
+            {
+                // DFS for every child of the parent node
+                DFS(childNode);
+            }
+
+            passedNodes.Add(parentNode);
+        }
+    }
+}
---
+++ C#Algorithms/ExamPrep-13.07.2022/13.07.2022/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+namespace _01.TwoMinutesToMidnight
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // use the forumula for binom coefficient to find the factorial of a given number
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            BigInteger binomalCoefficient = GetFactoial(n) / (GetFactoial(k) * GetFactoial(n - k));
+            Console.WriteLine(binomalCoefficient);
+        }
+
+        private static BigInteger GetFactoial(int n)
+        {
+            BigInteger factorial = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
+        }
+    }
+}
