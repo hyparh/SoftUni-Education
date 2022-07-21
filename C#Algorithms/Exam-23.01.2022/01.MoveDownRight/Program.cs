b1302b3bcabbc0ddfe77f18a110586a8d3a9c843
hyparh <hyparh@gmail.com>
2022-07-21 13:28:56 +0300

    Solved exam prep

---
+++ C#Algorithms/Exam-23.01.2022/01.MoveDownRight/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.MoveDownRight
+{
+    internal class Program
+    {
+        private static int[,] matrix;
+        private static Dictionary<string, long> cache = new Dictionary<string, long>();
+
+        static void Main()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            matrix = new int[rows, cols];
+
+            Console.WriteLine(FindNumberOfUniquePaths(rows, cols));
+        }
+
+        private static long FindNumberOfUniquePaths(int rows, int cols)
+        {
+            if (rows is 1 || cols is 1)
+            {
+                return 1;
+            }
+
+            var key = $"{rows}-{cols}";
+
+            if (cache.ContainsKey(key))
+            {
+                return cache[key];
+            }
+
+            var result = FindNumberOfUniquePaths(rows - 1, cols) + FindNumberOfUniquePaths(rows, cols - 1);
+            cache[key] = result;
+
+            return result;
+        }
+    }
+}
---
+++ C#Algorithms/Exam-23.01.2022/03.StringsMashup/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.StringsMashup
+{    
+    class Program
+    {
+        private static string str;
+        private static List<char> variations;
+
+        static void Main()
+        {
+            str = Console.ReadLine();
+            variations = new List<char>(str);
+
+            Variations(0);
+        }
+
+        private static void Variations(int permutationIdx)
+        {
+            if (permutationIdx >= variations.Count)
+            {
+                Console.WriteLine(String.Join("", variations));
+
+                return;
+            }
+
+            Variations(permutationIdx + 1);
+
+            if (char.IsLetter(variations[permutationIdx]))
+            {
+                variations[permutationIdx] = Swap(variations, permutationIdx);
+
+                Variations(permutationIdx + 1);
+
+                variations[permutationIdx] = Swap(variations, permutationIdx);
+            }
+        }
+
+        private static char Swap(List<char> elements, int index)
+        {
+            return char.IsLower(elements[index]) ? char.ToUpper(elements[index]) : char.ToLower(elements[index]);
+        }
+    }
+}
