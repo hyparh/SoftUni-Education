eac372405aff024e3f1677edec5bc85d80994791
hyparh <hyparh@gmail.com>
2022-07-23 12:47:04 +0300

    Tasks 1 and 3 solved

---
+++ C#Algorithms/Exam-30.01.2022/01.StringMashup/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace PermutationsWithoutRepetition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    namespace VariationsWithoutRepetition
+    {
+        internal class Program
+        {
+            private static int k;
+            private static string input;
+            private static string tempStr;
+            private static char[] charArr;
+            private static char[] combinations;
+            private static List<string> result;
+
+            static void Main()
+            {
+                result = new List<string>();
+
+                input = Console.ReadLine();
+                charArr = input.ToCharArray();
+
+                k = int.Parse(Console.ReadLine());
+
+                combinations = new char[k];
+
+                Combinations(0, 0);
+
+                result.Sort();
+
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            static void Combinations(int idx, int elementsStartIdx)
+            {
+                if (idx >= combinations.Length)
+                {
+                    var rdyCombs = combinations
+                        .OrderBy(x => x)
+                        .ToArray();
+
+                    tempStr = new string(rdyCombs);
+                    result.Add(tempStr);
+
+                    return;
+                }
+
+                for (int i = elementsStartIdx; i < charArr.Length; i++)
+                {
+                    var res = combinations.ToString();
+
+                    combinations[idx] = charArr[i];
+                    Combinations(idx + 1, i);
+                }
+            }
+        }
+    }
+}
---
+++ C#Algorithms/Exam-30.01.2022/03.BankRobbery/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.DividingPresents
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var presents = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var allSums = FindAllSums(presents);
+
+            var totalSum = presents.Sum();
+            var alanSum = totalSum / 2;
+            var bobSum = totalSum - alanSum;
+
+            while (true)
+            {
+                if (allSums.ContainsKey(alanSum))
+                {
+                    var alanPresents = FindAlanSubset(allSums, alanSum)
+                        .OrderBy(x => x);
+
+                    var bobPresents = presents
+                        .Except(alanPresents)
+                        .OrderBy(x => x)
+                        .ToArray();
+
+                    Console.WriteLine($"{String.Join(" ", alanPresents)}");
+                    Console.WriteLine($"{String.Join(" ", bobPresents)}");
+
+                    break;
+                }
+
+                alanSum -= 1;
+            }
+        }
+
+        private static List<int> FindAlanSubset(Dictionary<int, int> sums, int target)
+        {
+            var subsetAlan = new List<int>();
+
+            while (target != 0)
+            {
+                var element = sums[target];
+                subsetAlan.Add(element);
+
+                target -= element;
+            }
+
+            return subsetAlan;
+        }
+
+        private static Dictionary<int, int> FindAllSums(int[] elements)
+        {
+            var sums = new Dictionary<int, int>() { { 0, 0 } };
+
+            foreach (var element in elements)
+            {
+                var currentSums = sums.Keys.ToArray();
+
+                foreach (var sum in currentSums)
+                {
+                    var newSum = sum + element;
+
+                    if (sums.ContainsKey(newSum))
+                    {
+                        continue;
+                    }
+
+                    sums.Add(newSum, element);
+                }
+            }
+
+            return sums;
+        }
+    }
+}
