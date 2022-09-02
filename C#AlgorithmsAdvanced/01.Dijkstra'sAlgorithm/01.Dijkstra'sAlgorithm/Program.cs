16f0bf4ce01c14783a2e1872161b83d00c5a1e82
hyparh <hyparh@gmail.com>
2022-09-02 22:38:10 +0300

    Solved all 3 tasks from the first lecture

---
+++ C#AlgorithmsAdvanced/01.Dijkstra'sAlgorithm/01.Dijkstra'sAlgorithm/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace _01.Dijkstra_sAlgorithm
+{
+    public class Edge
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+    }
+    public class Program
+    {
+        private static Dictionary<int, List<Edge>> edgesByNode;
+        private static double[] distance;
+        private static int[] parent;
+
+        static void Main()
+        {
+            edgesByNode = new Dictionary<int, List<Edge>>();
+
+            var edgesCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < edgesCount; i++)
+            {
+                var edgeArgs = Console.ReadLine()
+                    .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var firstNode = edgeArgs[0];
+                var secondNode = edgeArgs[1];
+
+                var edge = new Edge
+                {
+                    First = firstNode,
+                    Second = secondNode,
+                    Weight = edgeArgs[2]
+                };
+
+                if (!edgesByNode.ContainsKey(firstNode))
+                {
+                    edgesByNode.Add(firstNode, new List<Edge>());
+                }
+
+                if (!edgesByNode.ContainsKey(secondNode))
+                {
+                    edgesByNode.Add(secondNode, new List<Edge>());
+                }
+
+                edgesByNode[firstNode].Add(edge);
+                edgesByNode[secondNode].Add(edge);
+            }
+
+            var biggestNode = edgesByNode.Keys.Max();
+
+            distance = new double[biggestNode + 1];
+
+            for (int node = 0; node < distance.Length; node++)
+            {
+                distance[node] = double.PositiveInfinity;
+            }
+
+            parent = new int[biggestNode + 1];
+
+            // fill the array with -1
+            for (int node = 0; node < parent.Length; node++)
+            {
+                parent[node] = -1;
+            }
+
+            var startNode = int.Parse(Console.ReadLine());
+            var endNode = int.Parse(Console.ReadLine());
+
+            distance[startNode] = 0;
+
+            var bag = new OrderedBag<int>(Comparer<int>.Create((f, s) => (int) (distance[f] - distance[s])));
+            bag.Add(startNode);
+
+            while (bag.Count > 0)
+            {
+                var minNode = bag.RemoveFirst();
+
+                if (double.IsPositiveInfinity(minNode))
+                {
+                    break;
+                }
+
+                // if we search for a single end node this if statement is good to use
+                // if we search for all other nodes it cannot be used
+                if (minNode == endNode)
+                {
+                    break;
+                }
+
+                foreach (var edge in edgesByNode[minNode])
+                {
+                    var otherNode = edge.First == minNode 
+                        ? edge.Second 
+                        : edge.First;
+
+                    if (double.IsPositiveInfinity(distance[otherNode]))
+                    {
+                        bag.Add(otherNode);
+                    }
+
+                    var newDistance = distance[minNode] + edge.Weight;
+
+                    if (newDistance < distance[otherNode])
+                    {
+                        parent[otherNode] = minNode;
+                        distance[otherNode] = newDistance;
+
+                        bag = new OrderedBag<int>(
+                            bag,
+                            Comparer<int>.Create((f, s) => (int)(distance[f] - distance[s])));
+                    }                    
+                }
+            }
+
+            if (double.IsPositiveInfinity(distance[endNode]))
+            {
+                Console.WriteLine("There is no such path.");
+            }
+            else
+            {
+                Console.WriteLine(distance[endNode]);
+
+                var currentNode = endNode;
+                var path = new Stack<int>();
+
+                while (currentNode != -1)
+                {
+                    path.Push(currentNode);
+                    currentNode = parent[currentNode];
+                }
+
+                Console.WriteLine(String.Join(" ", path));
+            }           
+        }
+    }
+}
---
+++ C#AlgorithmsAdvanced/01.Dijkstra'sAlgorithm/03.Prim'sAlgorithm/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace _03.Prim_sAlgorithm
+{
+    public class Edge
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class Program
+    {
+        private static Dictionary<int, List<Edge>> graph;
+        private static HashSet<int> forestNodes;
+        private static List<Edge> forestEdges;
+
+        static void Main()
+        {
+            graph = new Dictionary<int, List<Edge>>();
+            forestNodes = new HashSet<int>();
+            forestEdges = new List<Edge>();
+
+            var edges = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var firstNode = edgeData[0];
+                var secondNode = edgeData[1];
+
+                if (!graph.ContainsKey(firstNode))
+                {
+                    graph.Add(firstNode, new List<Edge>());
+                }
+
+                if (!graph.ContainsKey(secondNode))
+                {
+                    graph.Add(secondNode, new List<Edge>());
+                }
+
+                var edge = new Edge
+                {
+                    First = firstNode,
+                    Second = secondNode,
+                    Weight = edgeData[2]
+                };
+
+                graph[firstNode].Add(edge);
+                graph[secondNode].Add(edge);
+            }
+
+            foreach (var node in graph.Keys)
+            {
+                if (!forestNodes.Contains(node))
+                {
+                    Prim(node);
+                }
+            }
+
+            foreach (var edge in forestEdges)
+            {
+                Console.WriteLine($"{edge.First} - {edge.Second}");
+            }
+        }
+
+        private static void Prim(int startingNode)
+        {
+            forestNodes.Add(startingNode);
+
+            var bag = new OrderedBag<Edge>(
+                Comparer<Edge>.Create((f, s) => f.Weight - s.Weight));
+
+            bag.AddMany(graph[startingNode]);
+
+            while (bag.Count > 0)
+            {
+                var minEdge = bag.RemoveFirst();
+
+                var nonTreeNode = -1;
+
+                if (forestNodes.Contains(minEdge.First) && !forestNodes.Contains(minEdge.Second))
+                {
+                    nonTreeNode = minEdge.Second;
+                }
+
+                if (forestNodes.Contains(minEdge.Second) && !forestNodes.Contains(minEdge.First))
+                {
+                    nonTreeNode = minEdge.First;
+                }
+
+                if (nonTreeNode == -1)
+                {
+                    continue;
+                }
+
+                forestNodes.Add(nonTreeNode);
+                forestEdges.Add(minEdge);
+                bag.AddMany(graph[nonTreeNode]);
+            }
+        }
+    }
+}
