70480d945da6d4a23440a1b4e139326550525595
hyparh <hyparh@gmail.com>
2022-09-09 20:19:51 +0300

    All tasks solved

---
+++ C#AlgorithmsAdvanced/03.GraphsBellman-Ford-Exercise/03.GraphsBellman-Ford-Exercise/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+namespace _01.MostReliablePath
+{
+    public class Edge
+    {
+        public int First { get; set; }
+
+        public int Second { get; set; }
+
+        public int Weight { get; set; }
+    }
+
+    public class Program
+    {
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            var graph = new List<Edge>[nodes];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<Edge>();
+            }
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var first = edgeData[0];
+                var second = edgeData[1];
+                var weight = edgeData[2];
+
+                var edge = new Edge
+                {
+                    First = first,
+                    Second = second,
+                    Weight = weight
+                };
+
+                graph[first].Add(edge);
+                graph[second].Add(edge);
+            }
+
+            var source = int.Parse(Console.ReadLine());
+            var destination = int.Parse(Console.ReadLine());
+
+            var reliability = new double[graph.Length];
+            var prev = new int[graph.Length];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                reliability[node] = double.NegativeInfinity;
+                prev[node] = -1;
+            }
+
+            reliability[source] = 100;
+
+            var bag = new OrderedBag<int>(
+                Comparer<int>.Create((f, s) => reliability[s].CompareTo(reliability[f])));
+
+            bag.Add(source);
+
+            while (bag.Count > 0)
+            {
+                var node = bag.RemoveFirst();
+
+                if (node == destination)
+                {
+                    break;
+                }
+
+                foreach (var edge in graph[node])
+                {
+                    var child = edge.First == node 
+                        ? edge.Second 
+                        : edge.First;
+
+                    if (double.IsNegativeInfinity(reliability[child]))
+                    {
+                        bag.Add(child);
+                    }
+
+                    var newReliability = reliability[node] * edge.Weight / 100;
+
+                    if (newReliability > reliability[child])
+                    {
+                        reliability[child] = newReliability;
+                        prev[child] = node;
+                    }
+
+                    bag = new OrderedBag<int>(
+                        bag,
+                        Comparer<int>.Create((f, s) => reliability[s].CompareTo(reliability[f])));
+                }
+            }
+
+            Console.WriteLine($"Most reliable path reliability: {reliability[destination]:F2}%");
+
+            var path = new Stack<int>();
+
+            var currentNode = destination;
+
+            while (currentNode != -1)
+            {
+                path.Push(currentNode);
+                currentNode = prev[currentNode];
+            }
+
+            Console.WriteLine(String.Join(" -> ", path));
+        }
+    }
+}
---
+++ C#AlgorithmsAdvanced/03.GraphsBellman-Ford-Exercise/05.CableNetwork/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+namespace _05.CableNetwork
+{
+    public class Program
+    {
+        public class Edge
+        {
+            public int First { get; set; }
+
+            public int Second { get; set; }
+
+            public int Weight { get; set; }
+        }
+
+        // For this task we use Prim
+        static void Main()
+        {
+            var budget = int.Parse(Console.ReadLine());
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            var graph = new List<Edge>[nodes];
+            var spanningTree = new HashSet<int>();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<Edge>();
+            }
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine().Split();
+
+                var edge = new Edge 
+                {
+                    First = int.Parse(edgeData[0]),
+                    Second = int.Parse(edgeData[1]),
+                    Weight = int.Parse(edgeData[2])
+                };
+
+                graph[edge.First].Add(edge);
+                graph[edge.Second].Add(edge);
+
+                if (edgeData.Length == 4)
+                {
+                    spanningTree.Add(edge.First);
+                    spanningTree.Add(edge.Second);
+                }
+            }
+
+            var usedBudget = Prim(graph, spanningTree, budget);
+
+            Console.WriteLine($"Budget used: {usedBudget}");
+        }
+
+        private static int Prim(List<Edge>[] graph, HashSet<int> spanningTree, int budget)
+        {
+            var usedBudget = 0;
+
+            var bag = new OrderedBag<Edge>(
+                Comparer<Edge>.Create((f, s) => f.Weight.CompareTo(s.Weight)));
+
+            foreach (var node in spanningTree)
+            {
+                bag.AddMany(graph[node]);
+            }
+
+            while (bag.Count > 0)
+            {
+                var minEdge = bag.RemoveFirst();
+
+                var nonTreeNode = -1;
+
+                if (spanningTree.Contains(minEdge.First) && !spanningTree.Contains(minEdge.Second))
+                {
+                    nonTreeNode = minEdge.Second;
+                }
+
+                if (spanningTree.Contains(minEdge.Second) && !spanningTree.Contains(minEdge.First))
+                {
+                    nonTreeNode = minEdge.First;
+                }
+
+                if (nonTreeNode == -1)
+                {
+                    continue;
+                }
+               
+                if (usedBudget + minEdge.Weight > budget)
+                {
+                    break;
+                }
+
+                spanningTree.Add(nonTreeNode);
+                bag.AddMany(graph[nonTreeNode]);
+                usedBudget += minEdge.Weight;               
+            }
+
+            return usedBudget;
+        }
+    }
+}
