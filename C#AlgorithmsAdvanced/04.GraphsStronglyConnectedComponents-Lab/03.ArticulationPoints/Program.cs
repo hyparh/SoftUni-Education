26dc790414bd9345d4325e68c6fa715db5398e99
hyparh <hyparh@gmail.com>
2022-09-16 18:58:00 +0300

    All tasks solved

---
+++ C#AlgorithmsAdvanced/04.GraphsStronglyConnectedComponents-Lab/03.ArticulationPoints/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.ArticulationPoints
+{
+    public class Program
+    {
+        private static List<int>[] graph;
+        private static bool[] visited;
+        private static int[] depth;
+        private static int[] lowpoint;
+        private static int[] parent;
+
+        private static List<int> articulationPoints;
+
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[nodes];
+            visited = new bool[nodes];
+            depth = new int[nodes];
+            lowpoint = new int[nodes];
+            parent = new int[nodes];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<int>();
+                parent[node] = -1;
+            }
+
+            for (int l = 0; l < lines; l++)
+            {
+                var line = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var node = line[0];
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    var child = line[j];
+
+                    graph[node].Add(child);
+                    graph[child].Add(node);
+                }
+
+                graph[node].AddRange(line.Skip(1));
+            }
+
+            articulationPoints = new List<int>();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                FindArticulationPoints(node, 1);
+            }
+
+            Console.WriteLine($"Articulation points: {String.Join(", ", articulationPoints)}");
+        }
+
+        private static void FindArticulationPoints(int node, int currentDepth)
+        {
+            visited[node] = true;
+            depth[node] = currentDepth;
+            lowpoint[node] = currentDepth;
+
+            var childCount = 0;
+            var isArticulationPoint = false;
+
+            foreach (var child in graph[node])
+            {
+                if (!visited[child])
+                {
+                    parent[child] = node;
+                    FindArticulationPoints(child, currentDepth + 1);
+
+                    childCount += 1;
+
+                    if (lowpoint[child] >= depth[node])
+                    {
+                        isArticulationPoint = true;
+                    }
+
+                    lowpoint[node] = Math.Min(lowpoint[node], lowpoint[child]);
+                }
+                else if (parent[node] != child)
+                {
+                    lowpoint[node] = Math.Min(lowpoint[node], depth[child]); 
+                }
+            }
+
+            if ((parent[node] == -1 && childCount > 1) || (parent[node] != -1 && isArticulationPoint))
+            {
+                articulationPoints.Add(node);
+            }
+        }
+    }
+}
---
+++ C#AlgorithmsAdvanced/04.GraphsStronglyConnectedComponents-Lab/04.GraphsStronglyConnectedComponents-Lab/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.StronglyConnectedComponents
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
+
+            var graph = new List<int>[nodes];
+            var reversedGraph = new List<int>[nodes];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<int>();
+                reversedGraph[node] = new List<int>();
+            }
+
+            for (int i = 0; i < lines; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var node = line[0];
+
+                for (int j = 1; j < line.Length; j++)
+                {
+                    var child = line[j];
+
+                    graph[node].Add(child);
+                    reversedGraph[child].Add(node);
+                }
+            }
+
+            var visited = new bool[graph.Length];
+            var sorted = new Stack<int>();
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                DFS(node, graph, visited, sorted);
+            }
+
+            visited = new bool[graph.Length];
+
+            Console.WriteLine("Strongly Connected Components:");    
+
+            while (sorted.Count > 0)
+            {
+                var node = sorted.Pop();
+                var component = new Stack<int>();
+
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                DFS(node, reversedGraph, visited, component);
+
+                Console.WriteLine($"{{{String.Join(", ", component)}}}");
+            }
+        }
+
+        private static void DFS(int node, List<int>[] graph, bool[] visited, Stack<int> result)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in graph[node])
+            {
+                DFS(child, graph, visited, result);
+            }
+
+            result.Push(node);
+        }
+    }
+}
