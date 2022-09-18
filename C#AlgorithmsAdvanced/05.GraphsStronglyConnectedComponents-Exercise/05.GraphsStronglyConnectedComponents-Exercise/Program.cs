3f8da3ced0da1dba42f11f135d94f33866b3522e
hyparh <hyparh@gmail.com>
2022-09-18 16:19:54 +0300

    All tasks solved

---
+++ C#AlgorithmsAdvanced/05.GraphsStronglyConnectedComponents-Exercise/05.GraphsStronglyConnectedComponents-Exercise/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.GraphsStronglyConnectedComponents_Exercise
+{
+    public class Program
+    {
+        private static List<int>[] graph;
+        private static List<int>[] reversedGraph;
+        static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var lines = int.Parse(Console.ReadLine());
+
+            graph = new List<int>[nodes];
+            reversedGraph = new List<int>[nodes];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<int>();
+                reversedGraph[node] = new List<int>();
+            }
+
+            for (int l = 0; l < lines; l++)
+            {
+                var lineElements = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var node = lineElements[0];
+
+                for (int i = 1; i < lineElements.Length; i++)
+                {
+                    var child = lineElements[i];
+
+                    graph[node].Add(child);
+                    reversedGraph[child].Add(node);
+                }
+            }
+
+            var sorted = new Stack<int>();
+            var visited = new bool[graph.Length];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                DFS(node, graph, visited, sorted);
+            }
+
+            visited = new bool[graph.Length];
+
+            while (sorted.Count > 0)
+            {
+                var node = sorted.Pop();
+
+                if (visited[node])
+                {
+                    continue;
+                }
+
+                var component = new Stack<int>();
+
+                DFS(node, reversedGraph, visited, component);
+
+                Console.WriteLine(String.Join(", ", component));
+            }
+        }
+
+        private static void DFS(int node, List<int>[] targetGraph, bool[] visited, Stack<int> stack)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            foreach (var child in targetGraph[node])
+            {
+                DFS(child, targetGraph, visited, stack);
+            }
+
+            stack.Push(node);
+        }
+    }
+}
