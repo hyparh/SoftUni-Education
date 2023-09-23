f004602c4016b66dfd06137f5496b91965880fb0
hyparh <hyparh@gmail.com>
2023-09-23 14:18:32 +0300

    Add 404 page

--- JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
+++ JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
@@ -21,9 +21,14 @@ router.post("/create", (req, res) => {
   res.redirect("/");
 });
 
+//this here show perfectly the MVC pattern
 router.get("/:cubeId/details", (req, res) => {
   const cube = cubeService.getOne(req.params.cubeId);
-  
+
+  if (!cube) {
+    return res.redirect("/404");
+  }
+
   res.render("details", { cube });
 });
 
