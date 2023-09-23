60f5fac7c1fa84cd53a07dde175ffa22a8433db1
hyparh <hyparh@gmail.com>
2023-09-23 12:13:59 +0300

    Add cubeService with create logic

--- JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
+++ JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
@@ -1,13 +1,25 @@
 const router = require("express").Router(); //modular router
+const cubeService = require("../services/cubeService");
 
-router.get("/create", (req, res)  => {
-    res.render("create");
+router.get("/create", (req, res) => {
+    console.log(cubeService.getAll());
+  res.render("create");
 });
 
-router.post("/create", (req, res)  => {
-    console.log(req.body);
+router.post("/create", (req, res) => {
+  const { 
+    name, 
+    description, 
+    imageUrl, 
+    difficultyLevel 
+} = req.body; //destructuring the request
 
-    res.redirect("/");
+  cubeService.create({ 
+    name, 
+    description, 
+    imageUrl, 
+    difficultyLevel: Number(difficultyLevel) });
+  res.redirect("/");
 });
 
 module.exports = router;
