e110d04757c1385470ac159b4249076c05b2c613
hyparh <hyparh@gmail.com>
2023-10-01 12:28:47 +0300

    Show cube and accessories on attach page

--- JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
+++ JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router(); //modular router
 const cubeService = require("../services/cubeService");
+const accessoryService = require("../services/accessoryService");
 
 router.get("/create", (req, res) => {
   res.render("create");
@@ -32,8 +33,11 @@ router.get("/:cubeId/details", async (req, res) => {
   res.render("details", { cube });
 });
 
-router.get('/:cubeId/attach-accessory', (req, res) => {
-  res.render('accessory/attach');
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+  const cube = await cubeService.getOne(req.params.cubeId).lean();
+  const accessories = await accessoryService.getAll().lean();
+
+  res.render('accessory/attach', { cube, accessories });
 });
 
 module.exports = router;
