fea8e618653036013773b1e02a76320eba6add53
hyparh <hyparh@gmail.com>
2023-10-07 12:28:23 +0300

    Save user with validated password

--- JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/userController.js
+++ JSWeb/JS Back-End/Cubicle-Workshop/src/controllers/userController.js
@@ -1,7 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const userService = require("../services/userService");
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-})
+router.get("/register", (req, res) => {
+  res.render("users/register");
+});
+
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
+
+  await userService.register({ username, password, repeatPassword });
+
+  res.redirect("/users/login");
+});
 
 module.exports = router;
