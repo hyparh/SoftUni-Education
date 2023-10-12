b92f5fde8923405dc9a35591fe4ed733546018c0
hyparh <hyparh@gmail.com>
2023-10-12 22:39:40 +0300

    Set handlebars engine

--- JSWeb/JS Back-End/Exam-Petstagram/src/index.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 
 const routes = require("./routes");
 
 const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
