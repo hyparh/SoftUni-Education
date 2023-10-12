26a72e0aad8184738a086adb5753cb3767321c71
hyparh <hyparh@gmail.com>
2023-10-12 21:28:31 +0300

    Add static resources

--- JSWeb/JS Back-End/Exam-Petstagram/src/index.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
+
 const routes = require("./routes");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-
 app.use(routes);
 
 app.listen("5000", console.log("Listening on port 5000..."));
