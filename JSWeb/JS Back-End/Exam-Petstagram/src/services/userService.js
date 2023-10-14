4c487d63afee5b344b9f45c4e4ed2974a2d30921
hyparh <hyparh@gmail.com>
2023-10-14 18:16:50 +0300

    Validate if user already exists

--- JSWeb/JS Back-End/Exam-Petstagram/src/services/userService.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/services/userService.js
@@ -1,7 +1,12 @@
-const User = require('../models/User');
+const User = require("../models/User");
 
-exports.login = (username, password) => {
+exports.login = (username, password) => {};
 
+exports.register = async (userData) => {
+  const user = User.findOne({ userData: userData.username });
+  if (user) {
+    throw new Error('Username already exists');
+  }
+
+  return User.create(userData);
 };
-
-exports.register = (userData) => User.create(userData);
