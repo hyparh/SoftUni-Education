635a5bb9a4f5a12990e22484bb05a6a67d11008c
hyparh <hyparh@gmail.com>
2023-10-14 18:47:03 +0300

    Validate username and password

--- JSWeb/JS Back-End/Exam-Petstagram/src/services/userService.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/services/userService.js
@@ -1,11 +1,26 @@
 const User = require("../models/User");
 
-exports.login = (username, password) => {};
+const bcrypt = require("bcrypt");
+
+exports.login = async (username, password) => {
+  const user = await User.findOneByUsername({ username });
+
+  if (!user) {
+    throw new Error("Invalid username or password");
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error("Invalid username or password");
+  }
+
+  
+};
 
 exports.register = async (userData) => {
   const user = await User.findOne({ userData: userData.username });
   if (user) {
-    throw new Error('Username already exists');
+    throw new Error("Username already exists");
   }
 
   return User.create(userData);
