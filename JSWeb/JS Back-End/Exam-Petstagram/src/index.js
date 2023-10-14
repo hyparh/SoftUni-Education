01322355e550531db658af5ca526fe600dd42e01
hyparh <hyparh@gmail.com>
2023-10-14 17:21:47 +0300

    Setup mongoose

--- JSWeb/JS Back-End/Exam-Petstagram/src/index.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const routes = require("./routes");
 
 const app = express();
+
+//TODO: change DB name according to project name
+mongoose.connect(`mongodb://127.0.0.1:27017/petstagram`)
+    .then(() => console.log("DB connected successfully"))
+    .catch(err => console.log("DB error, ", err.message));
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
