303b93ed44bb9f0b2c7863017be303c3aaa5fe87
hyparh <hyparh@gmail.com>
2023-10-15 14:14:05 +0300

    Visualize list of pets

--- JSWeb/JS Back-End/Exam-Petstagram/src/controllers/photoController.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/controllers/photoController.js
@@ -3,6 +3,12 @@ const router = require("express").Router();
 const photoService = require("../services/photoService");
 const { getErrorMessage } = require("../utils/errorHelpers");
 
+router.get('/', async (req, res) => {
+    const photos = await photoService.getAll().lean();
+
+    res.render('photos', { photos });
+});
+
 router.get("/create", (req, res) => {
   res.render("photos/create"); //folder/handlebars file
 });
