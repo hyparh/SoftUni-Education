8dda4aca5b163db789fe0a80b5584cbb783da1a4
hyparh <hyparh@gmail.com>
2023-10-15 19:30:32 +0300

    Validation messages for models

--- JSWeb/JS Back-End/Exam-Petstagram/src/models/Photo.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/models/Photo.js
@@ -4,23 +4,33 @@ const photoSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Name is required"],
-    unique: true,
+    minLength: [2, "Name should be at least 2 characters long"]
   },
   image: {
     type: String,
     required: [true, "ImageUrl is required"],
+    match: {
+      regex: /^https?:\/\//,
+      message: "Invalid URL"
+    }
   },
   age: {
     type: Number,
     required: [true, "Age is required"],
+    min: [1, "Age should be at least 1"],
+    max: [100, "Age cannot be more than 100"]
   },
   description: {
     type: String,
     required: [true, "Description is required"],
+    minLength: [5, "Description should contain at least 5 characters long"],
+    maxLength: [50, "Description cannot contain more than 50 characters long"]
   },
   location: {
     type: String,
     required: [true, "Location is required"],
+    minLength: [5, "Location should contain at least 5 characters long"],
+    maxLength: [50, "Location cannot contain more than 50 characters long"]
   },
   owner: {
     type: mongoose.Types.ObjectId,
