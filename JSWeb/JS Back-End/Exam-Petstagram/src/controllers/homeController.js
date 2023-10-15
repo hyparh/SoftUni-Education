f1c42737ad0e44faef2db6d9f25bfd79ec8d2876
hyparh <hyparh@gmail.com>
2023-10-15 19:08:27 +0300

    bonus task (Profile)

--- JSWeb/JS Back-End/Exam-Petstagram/src/controllers/homeController.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/controllers/homeController.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
 
+const photoService = require("../services/photoService"); //for the bonus only
+
 router.get("/", (req, res) => {
   res.render("home");
 });
 
 router.get("/404", (req, res) => {
   res.render("404");
+});
+
+//this is for the bonus only - profile
+router.get("/profile", async (req, res) => {
+  const photos = await photoService.getByOwner(req.user._id).lean();
+
+  res.render("profile", { photos, photoCount: photos.length });
 });
 
 module.exports = router;
