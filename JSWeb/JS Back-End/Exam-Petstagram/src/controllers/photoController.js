5ec56b6931ad6fb8282d656a9d6e905e79b14d97
hyparh <hyparh@gmail.com>
2023-10-15 11:16:58 +0300

    photoController + custom error messages in model

--- JSWeb/JS Back-End/Exam-Petstagram/src/controllers/photoController.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/controllers/photoController.js
@@ -1,7 +1,22 @@
 const router = require("express").Router();
 
+const photoService = require("../services/photoService");
+const { getErrorMessage } = require("../utils/errorHelpers");
+
 router.get("/create", (req, res) => {
   res.render("photos/create"); //folder/handlebars file
+});
+
+router.post("/create", async (req, res) => {
+  const { photoData } = req.body;
+
+  try {
+    await photoService.create(photoData);
+
+    res.redirect("/photos");
+  } catch (err) {
+    res.render("photos/create", { error: getErrorMessage(err) }); //this is all necessary error handling
+  }
 });
 
 module.exports = router;
--- JSWeb/JS Back-End/Exam-Petstagram/src/models/Photo.js
+++ JSWeb/JS Back-End/Exam-Petstagram/src/models/Photo.js
@@ -3,24 +3,24 @@ const mongoose = require("mongoose");
 const photoSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Name is required"],
     unique: true,
   },
   image: {
     type: String,
-    required: true,
+    required: [true, "ImageUrl is required"],
   },
   age: {
     type: Number,
-    required: true,
+    required: [true, "Age is required"],
   },
   description: {
     type: String,
-    required: true,
+    required: [true, "Description is required"],
   },
   location: {
     type: String,
-    required: true,
+    required: [true, "Location is required"],
   },
   owner: {
     type: mongoose.Types.ObjectId,
