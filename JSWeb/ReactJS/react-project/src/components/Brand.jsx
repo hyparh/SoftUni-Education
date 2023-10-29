f11fbd81739d4b77a1b5e3a3534cc1ee5a996958
hyparh <hyparh@gmail.com>
2023-10-29 12:16:05 +0200

    Create components

---
+++ JSWeb/ReactJS/react-project/src/components/Brand.jsx
@@ -0,0 +1,51 @@
+export default function Brand() {
+  return (
+    <section id="brand" className="brand">
+      <div className="container">
+        <div className="brand-area">
+          <div className="owl-carousel owl-theme brand-item">
+            <div className="item">
+              <a href="#">
+                <img src="images/brand/br1.png" alt="brand-image" />
+              </a>
+            </div>
+            {/*/.item*/}
+            <div className="item">
+              <a href="#">
+                <img src="images/brand/br2.png" alt="brand-image" />
+              </a>
+            </div>
+            {/*/.item*/}
+            <div className="item">
+              <a href="#">
+                <img src="images/brand/br3.png" alt="brand-image" />
+              </a>
+            </div>
+            {/*/.item*/}
+            <div className="item">
+              <a href="#">
+                <img src="images/brand/br4.png" alt="brand-image" />
+              </a>
+            </div>
+            {/*/.item*/}
+            <div className="item">
+              <a href="#">
+                <img src="images/brand/br5.png" alt="brand-image" />
+              </a>
+            </div>
+            {/*/.item*/}
+            <div className="item">
+              <a href="#">
+                <img src="images/brand/br6.png" alt="brand-image" />
+              </a>
+            </div>
+            {/*/.item*/}
+          </div>
+          {/*/.owl-carousel*/}
+        </div>
+        {/*/.clients-area*/}
+      </div>
+      {/*/.container*/}
+    </section>
+  );
+}
